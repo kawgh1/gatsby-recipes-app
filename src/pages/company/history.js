0cf721a2335683f01a94e34c43ae9daf30137890
kawgh1 <krseven@gmail.com>
2021-12-28 21:19:01 -0600

    more pages

---
+++ src/pages/company/history.js
@@ -0,0 +1,7 @@
+import React from "react"
+
+function history() {
+  return <div>history of our company</div>
+}
+
+export default history
---
+++ src/pages/company/index.js
@@ -0,0 +1,7 @@
+import React from "react"
+
+function index() {
+  return <div>hello from company page</div>
+}
+
+export default index
